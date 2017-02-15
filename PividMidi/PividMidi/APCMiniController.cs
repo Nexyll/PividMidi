4b6f49ef3d06d5087c988bade4f55fbbb8c2d62e
Nexyll <antoine7104@gmail.com>
2017-02-15 19:50:45 +0100

    Ajout des classes de base

---
+++ PividMidi/PividMidi/APCMiniController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sanford.Multimedia.Midi;
+
+namespace PividMidi
+{
+    class APCMiniController
+    {
+        private InputDevice _inputDevice;
+        private OutputDevice _outputDevice;
+
+        public List<Control> Controls = new List<Control>();
+
+        public APCMiniController(InputDevice inputDevice, OutputDevice outputDevice)
+        {
+            _inputDevice = inputDevice;
+            _outputDevice = outputDevice;
+
+        }
+
+    }
+}
